eefb25f861c6d6d0b4e8317e4718a537ee60e63a
ST10449570 <ST10449570@rcconnect.edu.za>
2024-04-22 01:24:43 +0200

    First commit!

---
+++ src/javaapp_poe1/Class1.java
@@ -0,0 +1,110 @@
+
+package javaapp_poe1;
+
+class Class1 
+{
+    public boolean CheckUserName(String username)
+   {
+   if(username.length()<= 5)
+        {
+          System.out.print("Username successfully captured");
+        }
+   else
+        {
+         System.out.print("Username is not correctly formatted,please ensure that your username contains an underscore and is no more than 5 characters in length");
+        }  
+   if (username.contains("_"))
+        {
+            return true;
+        }
+   else
+        {
+            return false;
+        }
+       
+   }
+    
+    
+   public boolean CheckPasswordComplexity(String password)
+   {
+       if(password.length() > 8)
+    {
+   if(CheckPass(password))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+        return false;
+   }
+   
+   public boolean CheckPassword(String password)
+   {
+       boolean hasNum = false; boolean hasCap = false; char c;
+       for(int i = 0; i < password.length(); i++)
+       {
+           c = password.charAt(i);
+           if(Character.isDigit(c));
+           {
+               hasNum = true;
+           } 
+           
+           if(Character.isUpperCase(c))
+            {
+              hasCap = true;     
+            }
+           if(hasNum && hasCap)
+           {
+               return true;
+           }
+          
+       }
+        return false;
+   }
+        
+   
+   public boolean checkpasswordcomplexity(String password)
+   {
+      boolean hasSpecialChar = false;
+      String SpecialChar[] = {"@", "$", "!", "~", "#"};
+      
+      int count = 0;
+      while(count < SpecialChar.length)
+      {
+          if(password.contains(SpecialChar[count]))
+          {
+              hasSpecialChar = true;
+              count = SpecialChar.length;
+          }
+          count++;
+      }
+      return hasSpecialChar;
+    }
+ 
+   
+   public boolean RegisterUser(String username, String password)
+   {
+   
+   }
+   
+   public boolean LoginUser(String username, String password)
+   {
+     
+       
+   }
+   
+   public boolean ReturnLoginStatus()
+   {
+   
+   } 
+
+    private boolean CheckPass(String password) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
---
+++ src/javaapp_poe1/JavaApp_POE1.java
@@ -0,0 +1,87 @@
+
+package javaapp_poe1;
+import java.util.Scanner;
+public class JavaApp_POE1 
+{
+
+    public static void main(String[] args) 
+    {
+     Scanner m = new Scanner(System.in);
+       String firstname, lastname, username, password;      
+       boolean CUN,CPC, CP, cpc;
+       Class1 cs1 = new Class1();
+       
+        System.out.print("Enter  first name :");
+        firstname = m.next();
+        
+        System.out.print("Enter  last name : ");
+        lastname = m.next();
+        
+         
+        System.out.print("Enter username : ");
+        username = m.next();
+         
+         CUN = cs1.CheckUserName(username);
+         if(CUN == true)
+            {
+               System.out.print("Username successfully captured \n");
+               username = m.next();
+            }  
+            else
+            {
+               System.out.print(" \n"); 
+               username = m.next();
+            }
+        
+                 
+            System.out.print("Enter password : ");
+            password = m.next();
+            
+            CPC = cs1.CheckPasswordComplexity(password);
+            if(CPC == true)
+            {
+              System.out.print("Password successfully captured : ");
+              password = m.next();
+            }
+            else
+            {
+              System.out.print("Password is not correctly formatted,please ensure that the password contains \n atleast 8 characters, a capital letter,a number and a special character");
+               password = m.next();
+            }
+            
+            CP = cs1.CheckPassword(password);
+            if(CP == true)
+            {
+              System.out.print("Password successfully captured : ");
+               password = m.next();
+            }
+            else
+            {
+              System.out.print("Password is not correctly formatted,please ensure that the password contains \n atleast 8 characters, a capital letter,a number and a special character");
+               password = m.next();
+            }
+            
+            cpc = cs1.checkpasswordcomplexity(password);
+            if(cpc == true)
+             {
+              System.out.print(" \n Password successfully captured");
+               password = m.next();
+            }
+            else
+            {
+              System.out.print("Password is not correctly formatted,please ensure that the password contains \n atleast 8 characters, a capital letter,a number and a special character");
+               password = m.next();
+            }   
+           if((username.equals("Jay_")) && (password.equals("Evans#50")))
+           {
+               System.out.print("Welcome Jay_, it is great to see you again");
+           }
+            else
+           {
+               System.out.print("Username or password incorrect,please try again");
+           }
+           
+           
+    }
+    
+}
